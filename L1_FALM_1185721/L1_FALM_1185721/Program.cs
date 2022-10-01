a14e5ee0c9f38e80addd140437fb22bd5559a2e8
FidelLemus <fidelolo16@gmail.com>
2022-09-30 19:07:36 -0500

    aja

---
+++ L1_FALM_1185721/L1_FALM_1185721/Program.cs
@@ -0,0 +1,20 @@
+﻿class program
+{
+    static void Main()
+    {
+        string nombre;
+
+        Console.WriteLine("Ingrese su nombre");
+        nombre = Console.ReadLine();
+
+
+        Console.WriteLine("Hola mundo");
+        Console.WriteLine("Mi nombre es " + nombre);
+
+
+        Console.Write("Hola mundo");
+        Console.Write("Mi nombre es " + nombre);
+        Console.ReadKey();
+
+    }
+}
