a9cb950b93e16ec1c643da42ef838000ba3fe8b9
FidelLemus <fidelolo16@gmail.com>
2022-09-30 22:01:53 -0500

    Tarea 1

---
+++ T1_FAML_1185721/T1_FAML_1185721/Program.cs
@@ -0,0 +1,32 @@
+﻿class program
+{
+    static void Main()
+    {
+        string nombre, edad, carrera, carne;
+
+        Console.WriteLine("Mi segundo programa");
+
+        Console.WriteLine("Ingrese su nombre");
+        nombre = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su edad");
+        edad = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su carrera");
+        carrera = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su carné");
+        carne = Console.ReadLine();
+
+
+        Console.WriteLine("Nombre: "+ nombre);
+        Console.WriteLine("Edad: " +edad );
+        Console.WriteLine("Carrera: " + carrera);
+        Console.WriteLine("Carné: " + carne);
+
+        Console.WriteLine("Soy " + nombre + " tengo " + edad + " años y estudio la carrera de "+ carrera + ". " + " Mi numero de carné es "+ carne );
+
+        Console.ReadKey();
+
+    }
+}
